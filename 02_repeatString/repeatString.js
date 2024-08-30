c6034a815e4d33b70e9fa4ef461baf7f8f43109b
Marc Olata <marc.olata123@gmail.com>
2024-08-30 22:24:27 +0800

    Finished 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
+    return word.repeat(num)
+}
 
-};
+;
 
 // Do not edit below this line
 module.exports = repeatString;
+
+console.log(repeatString("hello",2))
