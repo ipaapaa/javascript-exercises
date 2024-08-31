3ca5efbdfb97f64c13dfaa9ccde3904b9f35908c
Marc Olata <marc.olata123@gmail.com>
2024-08-31 16:45:18 +0800

    Finished 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,37 @@
-const palindromes = function () {
+const palindromes = function (string) {
+
+    function letterVerification(character) {
+        // Use a regular expression to check if the character is not a letter
+        return /[a-zA-Z0-9]/.test(character);
+    }
+
+    function spaceVerification(character){
+        return (character !== " ");
+    }
+    
+    let stringArray = string.toLowerCase().split('');
+
+    stringArray = stringArray.filter(letterVerification);
+    stringArray = stringArray.filter(spaceVerification);
+
+    string = stringArray.join('')
+    let stringReverse = stringArray.reverse().join('')
+    
+    
+    console.log(string)
+    console.log(stringReverse)
+
+    if(stringReverse === string){
+        return true;
+    }
+    else{
+        return false;
+    }
+
 
 };
+
+console.log(palindromes("rac3e3car"))
 
 // Do not edit below this line
 module.exports = palindromes;
