c05446781806da5bfab319142951156f6628f593
Marc Olata <marc.olata123@gmail.com>
2024-08-31 09:48:08 +0800

    Finished 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  celsius = (fahrenheit - 32)*(5/9)
+
+  if(celsius % 1 !== 0){
+    return parseFloat(celsius.toFixed(1));}
+  else
+    return celsius
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  fahrenheit = (celsius*9/5)+32
+  
+  if(fahrenheit % 1 !== 0){
+    return parseFloat(fahrenheit.toFixed(1));}
+  else
+    return fahrenheit
 };
 
-// Do not edit below this line
+// Do not edit below thishow line
 module.exports = {
   convertToCelsius,
   convertToFahrenheit
