d58fcb1ce5eac953d2c2a74a24b082711a98c8fa
Marc Olata <marc.olata123@gmail.com>
2024-08-31 17:17:49 +0800

    Finished 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(nthNumber) {
+ 
+    nthNumber = parseInt(nthNumber);
+    if (nthNumber === 1 || nthNumber === 2){
+        return 1;
+    } else if (nthNumber < 0){
+        return "OOPS";
+    } else if(nthNumber === 0){
+        return 0;
+    }
+    else{
+        return fibonacci(nthNumber - 1) + fibonacci(nthNumber - 2)
+    }
+
+
+    
 
 };
 
